f20f997e51a31ef45ad758261452c956fa2c61f1
Jesus Velasco <j3zuzw@gmail.com>
2024-09-13 17:43:17 -0600

    property ordering and modification of getRandomColor function

--- src/components/InitialCircle.jsx
+++ src/components/InitialCircle.jsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  const randomHex = () => letters[Math.floor(Math.random() * letters.length)];
+
+  return `#${Array.from({ length: 6 }, randomHex).join('')}`;
 }
 
 class InitialCircle extends Component {
